0ae4f9e7b42457d184a0e542c2ce008f1c5cd84d
Viktor Lova <nsinreal@gmail.com>
2017-12-14 22:50:01 +0300

    coins: order portfolio by usd

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const { computeTotalBalance } = require("./fns/computeTotalBalance");
 const { getHitBtcEnhancedBalance } = require("./fns/getHitBtcEnhancedBalance");
 const { prettyWrite } = require("./common/helpers");
+const { orderBy } = require("lodash");
 
 async function main() {
     const balance = await getHitBtcEnhancedBalance();
     const totalBalance = computeTotalBalance(balance);
-    // prettyWrite(balance);
-    prettyWrite(balance.map(i => ({
+
+    prettyWrite(orderBy(balance.map(i => ({
         currency: i.currency,
         inUsd: i.exchanged.hitBtcReal.totalInUsd,
         inUsd2: i.exchanged.cryptoCompare.totalInUsd
-    })));
+    })), ['inUsd'], ['desc']));
 
     console.log();
-    
+
     prettyWrite({
         totalInUsd: totalBalance.hitBtcReal.totalInUsd,
         totalInUsd2: totalBalance.cryptoCompare.totalInUsd
