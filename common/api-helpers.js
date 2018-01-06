0efbd89c447b7815ee8c82d6bc66741516204a4d
Viktor Lova <nsinreal@gmail.com>
2018-01-07 01:23:57 +0300

    enhanced error handling

--- common/api-helpers.js
+++ common/api-helpers.js
@@ -6,23 +6,40 @@ var throttle = throttledQueue(15, 1000); // at most 15 requests per second.
 function getByUrl(url) {
     return new Promise(function (resolve, onError) {
         throttle(function () {
-            fetch(url, {}, (error, meta, body) => {
-                if (error != undefined) {
-                    onError(error);
-                    return;
-                }
+            try {
+                fetch(url, {}, (error, meta, body) => {
+                    if (error != undefined) {
+                        onError(error);
+                        return;
+                    }
 
-                try {
-                    const bodyStr = body.toString();
-                    resolve(JSON.parse(bodyStr));
-                }
-                catch (e) {
-                    onError({
-                        innerError: e,
-                        bodyStr: bodyStr
-                    });
-                }
-            })
+                    try {
+                        const bodyStr = body.toString();
+                        try {
+                            resolve(JSON.parse(bodyStr));
+                        }
+                        catch (e) {
+                            onError({
+                                errorType: 'api: parse error',
+                                innerError: e,
+                                bodyStr: bodyStr
+                            });
+                        }
+                    }
+                    catch (e) {
+                        onError({
+                            errorType: 'api: reading error',
+                            innerError: e
+                        });
+                    }
+                })
+            }
+            catch (e) {
+                onError({
+                    errorType: 'api: request error',
+                    innerError: e
+                });
+            }
         });
     });
 }
--- index.js
+++ index.js
@@ -4,30 +4,41 @@ const { prettyWrite, delay } = require("./common/helpers");
 const { orderBy } = require("lodash");
 
 async function main() {
-    const balance = await getHitBtcEnhancedBalance();
-    prettyWrite(orderBy(balance.map(i => ({
-        currency: i.currency,
-        inUsd: i.exchanged.hitBtcReal.totalInUsd,
-        inUsd2: i.exchanged.cryptoCompare.totalInUsd
-    })), ['inUsd'], ['desc']));
+    try {
+        const balance = await getHitBtcEnhancedBalance();
+        prettyWrite(orderBy(balance.map(i => ({
+            currency: i.currency,
+            inUsd: i.exchanged.hitBtcReal.totalInUsd,
+            inUsd2: i.exchanged.cryptoCompare.totalInUsd
+        })), ['inUsd'], ['desc']));
+    }
+    catch (e) {
+        console.error(e);
+    }
 
     console.log();
     console.log();
 
     while (true) {
-        const balance = await getHitBtcEnhancedBalance();
-        const totalBalance = computeTotalBalance(balance);
+        try {
+            const balance = await getHitBtcEnhancedBalance();
+            const totalBalance = computeTotalBalance(balance);
 
-        prettyWrite({
-            totalInUsd: totalBalance.hitBtcReal.totalInUsd,
-            totalInUsd2: totalBalance.cryptoCompare.totalInUsd,
-            date: new Date()
-        });
+            prettyWrite({
+                totalInUsd: totalBalance.hitBtcReal.totalInUsd,
+                totalInUsd2: totalBalance.cryptoCompare.totalInUsd,
+                date: new Date()
+            });
+        }
 
-        await delay(30 * 1000);
+        catch (e) {
+            console.error(e);
+        }
+
+        await delay(3 * 1000);
 
         console.log();
     }
 }
 
-main();
+main().catch(e => console.error(e));
